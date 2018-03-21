190cc486a6e06c35c51922757bc13e7390f45987
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-21 23:22:17 +0530

    learning mode added

--- app.py
+++ app.py
@@ -28,6 +28,14 @@ def get_bot_response():
     userText = request.json['msg']
     return jsonify(response=str(chatbot.get_response(userText)))
 
+@app.route("/api/learn", methods=['POST'])
+def get_bot_response():
+    conversation = request.json['msg']
+    if len(conversation) !=2:
+    	return jsonify(error="invalid no of conversations")
+    else:
+    	chatbot.train(conversations)
+    	return jsonify(response=str(chatbot.get_response(userText)))
 
 if __name__ == "__main__":
     app.run(debug=True)
