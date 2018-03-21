82d96fbb4add362573e0d366b7803c9bdc8ca822
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-22 01:21:35 +0530

    learning mode added

---
+++ post_data.py
@@ -0,0 +1,12 @@
+import json,requests
+image_path='/home/santhosh/svce_innovates/flask-chatterbot/Savior.jpeg'
+response = requests.post('https://stark-brook-57528.herokuapp.com/api/image_classifier',
+files={'image': open(image_path, 'rb')})
+print(response.json())
+
+api_key = 'acc_a8a69b84e97af11'
+api_secret = '97eed8248a51c9feaf65e172ee9010e8'
+response = requests.post('https://api.imagga.com/v1/content',
+auth=(api_key, api_secret),
+files={'image': open(image_path, 'rb')})
+print(response.json())
