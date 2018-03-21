2c46139dd12365d9d2428955b79afb278beb4b95
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-22 00:04:55 +0530

    transilator added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
 import urllib
+import json,requests
 app = Flask(__name__)
 
 chatbot=ChatBot(
@@ -37,6 +38,24 @@ def learn():
     else:
     	chatbot.train(conversation)
     	return jsonify(response="success")
+
+headers = {
+    "Content-Type": "application/json",
+}
+
+@app.route("/api/translate", methods=['POST'])
+def translate():
+    #print(request.json['msg'])
+	requestPayload = {
+	    "to": email_data['to'],
+	    "from": "admin@project.com",
+	    "fromName": "SVCE Grievance Redressal Committee",
+	    "sub": email_data["sub"],
+	    "text": email_data["text"],
+	    "html": email_data["html"]
+	}
+	resp = requests.request("POST","https://translate.yandex.net/api/v1.5/tr.json/translate ?key=trnsl.1.1.20180321T182122Z.0744c55b609c1cca.0377b6bfa4fd2ff3b9efb061d71df78173b6a017& text="+request.json["text"]+"& lang="+request.json["from"]+"-"+request.json["to"], headers=headers)
+	return jsonify(json.loads(resp.content))
 
 if __name__ == "__main__":
     app.run(debug=True)
