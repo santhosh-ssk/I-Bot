dc5fe9d80525fbe29537e93d3878db6486d7753c
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-21 22:46:02 +0530

    chatterbot added

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
-
+import urllib
 app = Flask(__name__)
 
 chatbot=ChatBot(
     'i-Bot',
     storage_adapter = "chatterbot.storage.MongoDatabaseAdapter",
     database = "i-bot",
-    database_uri = "mongodb://Roy:Roy@123@ds121309.mlab.com:21309/i-bot",
+    database_uri = "mongodb://Roy:"+urllib.parse.quote("Roy@123")+"@ds121309.mlab.com:21309/i-bot",
     logic_adapters=[
         "chatterbot.logic.BestMatch",
         'chatterbot.logic.MathematicalEvaluation',
