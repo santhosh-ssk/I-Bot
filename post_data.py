ef80a99c2258906d67ce70f9d1d3975ec15cf887
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-22 07:40:47 +0530

    transilator added

--- post_data.py
+++ post_data.py
@@ -1,9 +1,9 @@
 import json,requests
 image_path='/home/santhosh/svce_innovates/flask-chatterbot/Savior.jpeg'
 response = requests.post('https://stark-brook-57528.herokuapp.com/api/image_classifier',
-files={'image': open(image_path, 'rb')})
+files={'image': open(image_path, 'r',encoding='utf-16')})
 print(response.json())
-files={'image': open(image_path, 'rb')}
+files={'image': open(image_path, 'r')}
 print(files)
 api_key = 'acc_a8a69b84e97af11'
 api_secret = '97eed8248a51c9feaf65e172ee9010e8'
