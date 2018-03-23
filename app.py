eca431fa1a235583bbdd6871c3a2caf5c55f8363
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-23 09:04:29 +0530

    jquery added

--- app.py
+++ app.py
@@ -81,7 +81,8 @@ def translate():
 def image_classifier():
 	api_key = 'acc_a8a69b84e97af11'
 	api_secret = '97eed8248a51c9feaf65e172ee9010e8'
-	image = request.files['image']
+	image = request.files['file']
+    print(image)
 	print(image.read().decode('utf-8'))
 	response = requests.post('https://api.imagga.com/v1/content',
 	auth=(api_key, api_secret),
