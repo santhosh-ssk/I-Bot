900efdb0b1154a758376ee163ba6b88123fa6044
S Santhosh Kumar <santhoshkumar.ssk3222@gmail.com>
2018-03-23 09:09:08 +0530

    jquery added

--- app.py
+++ app.py
@@ -82,8 +82,6 @@ def image_classifier():
 	api_key = 'acc_a8a69b84e97af11'
 	api_secret = '97eed8248a51c9feaf65e172ee9010e8'
 	image = request.files['file']
-
-    print(image)
 	print(image.read().decode('utf-8'))
 	response = requests.post('https://api.imagga.com/v1/content',
 	auth=(api_key, api_secret),
